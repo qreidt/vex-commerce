1ae5fb2399f9136efe6c4ff5bb64e05f9540b881
Caio Reidt <caioreidt@gmail.com>
2021-06-12 16:20:02 -0300

    NestJS 7.6.0

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+// noinspection JSIgnoredPromiseFromCall
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+	await app.listen(3000);
+}
+
+bootstrap();
