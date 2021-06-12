931c6e90af711386829b6e997cd37fd9323c8f2f
Caio Reidt <caioreidt@gmail.com>
2021-06-12 17:23:34 -0300

    Criar Migration de Usuários

---
+++ ormconfig.ts
@@ -0,0 +1,15 @@
+module.exports = {
+	type: process.env.DB_CLIENT ?? 'mariadb',
+	host: process.env.DB_HOST ?? 'localhost',
+	port: parseInt(process.env.DB_PORT) ?? 3306,
+	username: process.env.DB_USER ?? 'root',
+	password: process.env.DB_PASS ?? '',
+	database: process.env.DB_NAME ?? 'commerce',
+	migrationsTableName: 'migrations',
+	entities: ['src/models/*.ts'],
+	migrations: ['src/database/migrations/*.ts'],
+	cli: {
+		migrationsDir: 'src/database/migrations',
+	},
+	logging: process.env.DB_LOGGING !== 'false',
+};
---
+++ src/database/migrations/1623526534508-create_users_table.ts
@@ -0,0 +1,66 @@
+import {
+	MigrationInterface,
+	QueryRunner,
+	Table,
+	TableIndex,
+} from 'typeorm';
+
+export class createUsersTable1623526534508 implements MigrationInterface {
+	private table_name = 'users';
+
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: this.table_name,
+				columns: [
+					{
+						name: 'id',
+						type: 'bigint unsigned',
+						isPrimary: true,
+						isGenerated: true,
+						generationStrategy: 'increment',
+					},
+					{
+						name: 'email',
+						type: 'varchar',
+					},
+					{
+						name: 'password',
+						type: 'varchar',
+					},
+					{
+						name: 'name',
+						type: 'varchar',
+					},
+					{
+						name: 'type',
+						type: 'int',
+					},
+					{
+						name: 'created_at',
+						type: 'timestamp',
+						isNullable: true,
+					},
+					{
+						name: 'updated_at',
+						type: 'timestamp',
+						isNullable: true,
+					},
+				],
+			}),
+		);
+
+		await queryRunner.createIndex(
+			this.table_name,
+			new TableIndex({
+				name: `${this.table_name}_email_unique`,
+				columnNames: ['email'],
+				isUnique: true,
+			}),
+		);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable(this.table_name);
+	}
+}
