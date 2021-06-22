94fa164805f33cd13975f0d07cc79000037a2096
Caio Reidt <caioreidt@gmail.com>
2021-06-21 21:42:27 -0300

    Ajustar Autorização de Rotas

--- src/app/products/products.controller.ts
+++ src/app/products/products.controller.ts
@@ -7,40 +7,54 @@ import {
 	Param,
 	Delete,
 	HttpCode,
+	UseInterceptors,
+	ClassSerializerInterceptor,
+	UseGuards,
 } from '@nestjs/common';
 import ProductsService from './products.service';
 import CreateProductDto from './dto/create-product.dto';
 import UpdateProductDto from './dto/update-product.dto';
+import LocalAuthGuard from '../../common/guards/auth.guard';
 
 @Controller('products')
 export default class ProductsController {
 	constructor(private readonly service: ProductsService) {}
 
+	@UseGuards(LocalAuthGuard)
 	@Get('/')
+	@UseInterceptors(ClassSerializerInterceptor)
 	@HttpCode(200)
 	index() {
 		return this.service.list();
 	}
 
+	@UseGuards(LocalAuthGuard)
 	@Post('/')
+	@UseInterceptors(ClassSerializerInterceptor)
 	@HttpCode(201)
 	create(@Body() data: CreateProductDto) {
 		return this.service.create(data);
 	}
 
+	@UseGuards(LocalAuthGuard)
 	@Get('/:id')
+	@UseInterceptors(ClassSerializerInterceptor)
 	@HttpCode(200)
 	show(@Param('id') id: number) {
 		return this.service.findOne(+id);
 	}
 
+	@UseGuards(LocalAuthGuard)
 	@Patch('/:id')
+	@UseInterceptors(ClassSerializerInterceptor)
 	@HttpCode(200)
 	update(@Param('id') id: number, @Body() data: UpdateProductDto) {
 		return this.service.update(+id, data);
 	}
 
+	@UseGuards(LocalAuthGuard)
 	@Delete('/:id')
+	@UseInterceptors(ClassSerializerInterceptor)
 	@HttpCode(200)
 	remove(@Param('id') id: number) {
 		return this.service.remove(+id);
--- src/app/variants/variants.controller.ts
+++ src/app/variants/variants.controller.ts
@@ -7,40 +7,54 @@ import {
 	Param,
 	Delete,
 	HttpCode,
+	UseGuards,
+	UseInterceptors,
+	ClassSerializerInterceptor,
 } from '@nestjs/common';
 import VariantsService from './variants.service';
 import CreateVariantDto from './dto/create-variant.dto';
 import UpdateVariantDto from './dto/update-variant.dto';
+import LocalAuthGuard from '../../common/guards/auth.guard';
 
 @Controller('/variants')
 export default class VariantsController {
 	constructor(private readonly variantsService: VariantsService) {}
 
+	@UseGuards(LocalAuthGuard)
 	@Get('/')
+	@UseInterceptors(ClassSerializerInterceptor)
 	@HttpCode(200)
 	index() {
 		return this.variantsService.index();
 	}
 
+	@UseGuards(LocalAuthGuard)
 	@Post('/')
+	@UseInterceptors(ClassSerializerInterceptor)
 	@HttpCode(201)
 	create(@Body() data: CreateVariantDto) {
 		return this.variantsService.create(data);
 	}
 
+	@UseGuards(LocalAuthGuard)
 	@Get('/:id')
+	@UseInterceptors(ClassSerializerInterceptor)
 	@HttpCode(200)
 	show(@Param('id') id: number) {
 		return this.variantsService.findOne(+id);
 	}
 
+	@UseGuards(LocalAuthGuard)
 	@Patch('/:id')
+	@UseInterceptors(ClassSerializerInterceptor)
 	@HttpCode(200)
 	update(@Param('id') id: number, @Body() data: UpdateVariantDto) {
 		return this.variantsService.update(+id, data);
 	}
 
+	@UseGuards(LocalAuthGuard)
 	@Delete('/:id')
+	@UseInterceptors(ClassSerializerInterceptor)
 	@HttpCode(200)
 	remove(@Param('id') id: number) {
 		return this.variantsService.remove(+id);
